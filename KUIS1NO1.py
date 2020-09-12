3d2126ed110f4c0569b8a056aed2d15775fd2f5c
nurullailii <nurullaili22@gmail.com>
2020-09-11 19:31:05 -1200

    Fungsi pada phyton
    Penerapan def, lambda, dan class

--- KUIS1NO1.py
+++
@@ -1,7 +0,0 @@
-#KUIS 1 NOMOR 1
-#MBUATLAH SEBUAH DEF UNTUK MENGHITUNG VOLUME SEBUAH TABUNG YANG MEMILIKI DIAMETER 20 CM DAN TINGGI 28CM
-print("VOLUME TABUNG")
-def vtabung (d,t):
-    hasil= 3.14*0.25*d**2*t
-    print ("volume tabung adalah "+ str(hasil))
-vtabung(20,28)
